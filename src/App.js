90ac074216679ce89415c02b9ecae57e14dc7d76
vincitfx <vincitfx@gmail.com>
2024-04-24 18:27:16 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState, useEffect } from 'react';
+
+function App() {
+  useEffect(() => {
+    fetch('https://localhost:5001/weatherforecast')
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+      });
+  });
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
