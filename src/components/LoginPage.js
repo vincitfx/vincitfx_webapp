70076d49f8c5b2eb3142b83a488a31314b58c6fb
vincitfx <vincitfx@gmail.com>
2024-05-11 10:59:50 +0530

    11/05/2024

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,6 +1,13 @@
 import '../css/LoginPage.css'
 import { useNavigate } from 'react-router-dom';
-
+import logoimg from '../logo.png';
+import facebook from '../images/facebook.svg'
+import instagram from '../images/instagram.svg'
+import linkedin from '../images/linkedin.svg'
+import twitter from '../images/twitter.svg'
+import youtube from '../images/youtube.svg'
+import backarrowimgblack from '../images/backarrowimgblack.svg';
+import backarrowimgwhite from '../images/backarrowimgwhite.svg';
 
 function LoginPage()
 {
@@ -10,10 +17,15 @@ function LoginPage()
     const goToSignUpPage = ()=>{
         navigate('/signup');
     }
+    
+    const goToHomePage = ()=>{
+        navigate('/');
+    }
 
     return(
         <div className="login-main-div">
             <div className="login-div-1">
+                <img src={backarrowimgblack} className='backarrowimgblack' alt="image" onClick={()=> goToHomePage()}></img>
                 <p className='login-text-1'>
                     Log in and<br />
                     <mark style={{color: '#FF914D', backgroundColor : "white", backgroundColor:'rgba(0, 0, 0, 0)' }}>Trade with VincitFX</mark>
@@ -25,7 +37,15 @@ function LoginPage()
                 <a className='login-p-2' onClick={() => goToSignUpPage()}>Sign up</a>
             </div>
             <div className="login-div-2">
-
+                <img src={backarrowimgwhite} className='backarrowimgwhite' alt="image" onClick={()=> goToHomePage()}></img>
+                <img src={logoimg} className='logo-icon-singup' alt="image"></img>
+                <p className="app-name-signup">VincitFX</p>
+                <p className='login-p-3'>Join us <mark style={{color: '#FF914D', backgroundColor : "white", backgroundColor:'rgba(0, 0, 0, 0)' }}>Today!</mark></p>
+                <img src={facebook} id='facebook-icon' className='socialmedia-icon' alt="followus"></img>
+                <img src={instagram} id='instagram-icon' className='socialmedia-icon' alt="followus"></img>
+                <img src={linkedin} id='linkedin-icon' className='socialmedia-icon' alt="followus"></img>
+                <a href='https://twitter.com/VincitFX' target='_blank'><img src={twitter} id='twitter-icon' className='socialmedia-icon'alt="followus"></img></a>
+                <img src={youtube} id='youtube-icon' className='socialmedia-icon' alt="followus"></img>
             </div>
         </div>
     )
